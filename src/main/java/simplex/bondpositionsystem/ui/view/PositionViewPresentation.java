39d991af7aaa3927c2f791faf27a149658aa7f44
Yumi Yoshino <free6s.lol@gmail.com>
2019-03-30 23:47:30 +0900

    sortの際、ラムダ式の書き方を教えてもらいました。

--- src/main/java/simplex/bondpositionsystem/ui/view/PositionViewPresentation.java
+++ src/main/java/simplex/bondpositionsystem/ui/view/PositionViewPresentation.java
@@ -18,19 +18,19 @@ public class PositionViewPresentation {
     public void doPresentation() {
         String[] table = {"code", "name", "rate", "maturity", "couponCount", "amount", "bookValue", "marketPrice", "profitAndLoss"};
         for (int i = 0; i < table.length; i++) {
-            System.out.print("|" + String.format("%1$-12s", table[i]));
+            System.out.print("|" + String.format("%1$-16s", table[i]));
         }
         System.out.println("|");
 
         List<PositionView> positions = positionViewService.positionView();
         for (PositionView position : positions) {
             if (position.getMarketPrice() == null && position.getProfitAndLoss() == null) {
-                System.out.println(String.format("|%-12s|%-12s|%12s|%12d|%12d|%12s|%12s|%-12s|%-12s|",
+                System.out.println(String.format("|%-16s|%-16s|%16s|%16d|%16d|%16s|%16s|%-16s|%-16s|",
                         position.getCode(), position.getName(), position.getRate().setScale(3, RoundingMode.DOWN), position.getMaturity(), position.getCouponCount(),
                         position.getAmount().setScale(3, RoundingMode.DOWN), position.getBookValue().setScale(3, RoundingMode.DOWN),
                         "NA", "NA"));
             } else {
-                System.out.println(String.format("|%-12s|%-12s|%12s|%12d|%12d|%12s|%12s|%12s|%12s|",
+                System.out.println(String.format("|%-16s|%-16s|%16s|%16d|%16d|%16s|%16s|%16s|%16s|",
                         position.getCode(), position.getName(), position.getRate().setScale(3, RoundingMode.DOWN), position.getMaturity(), position.getCouponCount(),
                         position.getAmount().setScale(3, RoundingMode.DOWN), position.getBookValue().setScale(3, RoundingMode.DOWN),
                         position.getMarketPrice().setScale(3, RoundingMode.DOWN), position.getProfitAndLoss().setScale(3, RoundingMode.DOWN)));
